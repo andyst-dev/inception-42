f8fe7cad83ecc3600d21ee60553fd70749d1af28
andy <andy>
2025-11-18 10:26:16 +0100

    okkkkk

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -5,9 +5,8 @@ if [ ! -f /etc/nginx/ssl/nginx.crt ]; then
 	openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 		-keyout /etc/nginx/ssl/nginx.key \
 		-out /etc/nginx/ssl/nginx.crt \
-		-subj "/C=FR/ST=France/L=Paris/O=42/CN=${DOMAIN_NAME:-astoll.42.fr}" 2>/dev/null
+		-subj "/C=FR/ST=France/L=Paris/O=42/CN=${DOMAIN_NAME}" 2>/dev/null
 fi
 
 # lance NGINX en foreground (PID 1)
 exec nginx -g "daemon off;"
-
